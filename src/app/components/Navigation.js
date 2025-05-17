d6d33ee43302f56739af97aaa5ef848acd809fd8
umut <cengizumut31@gmail.com>
2025-05-17 19:36:58 +0300

    Update: Increase logo height to 100px

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -74,7 +74,7 @@ export default function Navigation() {
               width={600}
               height={150}
               priority
-              className="h-16 md:h-16 w-auto object-contain"
+              className="h-[100px] w-auto object-contain"
             />
           </Link>
 
