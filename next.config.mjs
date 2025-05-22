e55e31b9666a821e8fc2ca7623f314d846007579
umut <cengizumut31@gmail.com>
2025-05-22 19:35:50 +0300

    Fix project images configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    domains: ['hollandveranda.com'],
+    unoptimized: false,
   },
   eslint: {
     ignoreDuringBuilds: true,
